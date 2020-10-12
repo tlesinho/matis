43975a64ddf0c414b39790c84bf303cffcf09331
tlesinho <tles.usserbayev@gmail.com>
2020-10-12 19:59:31 +0800

    remove project reactor

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,9 +19,7 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
@@ -30,7 +28,6 @@ dependencies {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 	}
 	testImplementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo")
-	testImplementation("io.projectreactor:reactor-test")
 }
 
 tasks.withType<Test> {
--- src/main/kotlin/com/petserver/matis/controller/TeamController.kt
+++ src/main/kotlin/com/petserver/matis/controller/TeamController.kt
@@ -1,17 +1,18 @@
 package com.petserver.matis.controller
 
 import com.petserver.matis.properties.OddsProperties
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import reactor.core.publisher.Mono
 
 
 @RestController
 class TeamController(private val oddsProperties: OddsProperties) {
 
     @GetMapping("/team")
-    private fun getEmployeeById(@RequestParam team: String): Mono<String> {
-        return Mono.just("Hello $team!")
+    private fun getEmployeeById(@RequestParam team: String): Flow<String> {
+        return flowOf("Hello $team!")
     }
 }
